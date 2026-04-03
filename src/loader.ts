08ea554061484e4503a16b23572a6f6d4136f897
danieladane2196@gmail.com <danieladane2196@gmail.com>
2026-04-03 08:29:29 +0300

    update GurshaPlus script loader

--- src/loader.ts
+++ src/loader.ts
@@ -2,7 +2,7 @@
  * GurshaPlus Script Loader Utility
  */
 
-const GURSHA_SCRIPT_URL = "https://staging.gurshaplus.com/widget/widget.js";
+const GURSHA_SCRIPT_URL = "https://staging.gurshaplus.com/widget/widget.min.js";
 
 declare global {
   interface Window {
